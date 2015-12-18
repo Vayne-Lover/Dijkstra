c066aabe5a136cc2bb6c912a9979316bc1b12a80
姜况童 <Vayne-Lover@jiangkuangtongdemacbook.local>
2015-12-18 13:52:43 +0800

    Initial Commit

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dijkstra
+//
+//  Created by 姜况童 on 15/12/18.
+//  Copyright © 2015年 姜况童. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
