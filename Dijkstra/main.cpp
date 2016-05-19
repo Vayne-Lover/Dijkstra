4d6574783186a2cf1c87a9395439d68f6265d381
Vayne-Lover <406378362@qq.com>
2016-05-19 12:02:24 +0800

    Dijkstra algorithm with C++

--- Dijkstra/main.cpp
+++ Dijkstra/main.cpp
@@ -2,14 +2,36 @@
 //  main.cpp
 //  Dijkstra
 //
-//  Created by 姜况童 on 15/12/18.
-//  Copyright © 2015年 姜况童. All rights reserved.
+//  Created by 姜况童 on 16/5/19.
+//  Copyright © 2016年 姜况童. All rights reserved.
 //
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "Dijkstra.hpp"
+int main() {
+    int distance[5];
+    int preVertex[5];
+    
+    for (int i = 0; i < 5; ++i)
+    {
+        Dijkstra(5, i, map, distance, preVertex);
+        for (int j = 0; j < 5; ++j)
+        {
+            int index = j;
+            stack<int> trace;
+            while (preVertex[index] != -1) {
+                trace.push(preVertex[index]);
+                index = preVertex[index];
+            }
+            
+            cout << "路径：";
+            while (!trace.empty()) {
+                cout << trace.top() << " -- ";
+                trace.pop();
+            }
+            cout << j;
+            cout << " 距离是：" << distance[j] << endl;
+            
+            
+        }
+    }
     return 0;
 }
